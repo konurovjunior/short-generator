e411586ea30194800988618acf5e40fc2fea47c2
m.konurov <m.konurov@geeksoft.kz>
2024-04-28 20:25:34 +0500

    correct the order of narrations and backgorunds

--- video.py
+++ video.py
@@ -23,14 +23,21 @@ output_file = 'vertical_video.avi'
 out = cv2.VideoWriter(output_file, fourcc, frame_rate, (width, height))
 
 image_paths = glob.glob(os.path.join("images", '*'))
+image_paths = sorted(image_paths)
 full_narration  = AudioSegment.empty()
 
-for i in range(len(image_paths) - 1):
+for i, image in enumerate(image_paths):
     image1 = cv2.imread(image_paths[i])
-    image2 = cv2.imread(image_paths[i+1])
+    if i < len(image_paths) - 1:
+        image2 = cv2.imread(image_paths[i+1])
+    else:
+        image2 = cv2.imread(image_paths[0])
 
     narration = os.path.join("narrations", f"narration_{i+1}.mp3")
     duration = get_audio_duration(narration) - fade_time
+
+    if i > 0 or i == len(image_paths) - 1:
+        duration -= fade_time
 
     full_narration += AudioSegment.from_file(narration)
 
@@ -46,15 +53,6 @@ for i in range(len(image_paths) - 1):
         vertical_video_frame[:image1.shape[0], :] = blended_image
 
         out.write(vertical_video_frame)
-
-image1 = cv2.imread(image_paths[i+1])
-image2 = cv2.imread(image_paths[0])
-for alpha in np.linspace(0, 1, math.ceil(fade_time/1000*30)):
-    blended_image = cv2.addWeighted(image1, 1 - alpha, image2, alpha, 0)
-    vertical_video_frame = np.zeros((height, width, 3), dtype=np.uint8)
-    vertical_video_frame[:image1.shape[0], :] = blended_image
-
-    out.write(vertical_video_frame)
 
 out.release()
 cv2.destroyAllWindows()
