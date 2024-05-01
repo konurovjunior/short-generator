d9b05fa83656998807435731360b0d82f3d0842a
m.konurov <m.konurov@geeksoft.kz>
2024-05-01 19:53:59 +0500

    making subtitles from the actual text and word by word

--- text.py
+++ text.py
@@ -1,4 +1,43 @@
 import cv2
+import json
+from pydub import AudioSegment
+import os
+import math
+
+def get_audio_duration(audio_file):
+    audio = AudioSegment.from_file(audio_file)
+    duration_in_ms = len(audio)
+    return duration_in_ms
+
+def write_text(text, frame, video_writer):
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    white_color = (255, 255, 255)
+    black_color = (0, 0, 0)
+    thickness = 10
+    font_scale = 3
+    stroke = 10
+
+    text_size = cv2.getTextSize(text, font, font_scale, thickness)[0]
+    text_x = (frame.shape[1] - text_size[0]) // 2
+    text_y = (frame.shape[0] + text_size[1]) // 2
+    org = (text_x, text_y)
+
+    frame = cv2.putText(frame, text, org, font, font_scale, black_color, thickness + stroke, cv2.LINE_AA)
+    frame = cv2.putText(frame, text, org, font, font_scale, white_color, thickness, cv2.LINE_AA) 
+
+    video_writer.write(frame)
+
+def get_narrations() -> str:
+
+    with open("data.json") as f:
+        narration_data = json.load(f)
+
+    narrations = []
+    for element in narration_data:
+        if element["type"] == "text":
+            narrations.append(element)
+    
+    return narrations
 
 input_video = "final_video.avi"
 cap = cv2.VideoCapture(input_video)
@@ -7,30 +46,41 @@ fourcc = cv2.VideoWriter_fourcc(*'XVID')
 output_video = "with_transcript.avi"
 out = cv2.VideoWriter(output_video, fourcc, 30.0, (int(cap.get(3)), int(cap.get(4))))
 
-while cap.isOpened():
-    ret, frame = cap.read()
-    if not ret:
-        break
+narrations = get_narrations()
 
-    height, width, _ = frame.shape
+for i, narration in enumerate(narrations):
+    audio = os.path.join("narrations", f"narration_{i+1}.mp3")
+    duration = get_audio_duration(audio)
 
-    text = "this is a sample text"
-    font = cv2.FONT_HERSHEY_SIMPLEX
-    white_color = (255, 255, 255)
-    black_color = (0, 0, 0)
-    thickness = 10
-    font_scale = 3
-    stroke = 3
+    word_count = narration["content"].count(" ")
+    ms_per_word = duration / word_count
 
-    text_size = cv2.getTextSize(text, font, font_scale, thickness)[0]
-    text_x = (frame.shape[1] - text_size[0]) // 2
-    text_y = (frame.shape[0] + text_size[1]) // 2
-    org = (text_x, text_y)
+    words = narration["content"].split(" ")
+    for word in words:
+        for i in range(math.ceil(ms_per_word/1000*30)):
+            ret, frame = cap.read()
+            if not ret:
+                break 
+            write_text(word, frame, out)
+        
+# for i in range(5 * 30):
+#     ret, frame = cap.read()
+#     if not ret:
+#         break 
 
-    frame = cv2.putText(frame, text, org, font, font_scale, black_color, thickness + stroke, cv2.LINE_AA)
-    frame = cv2.putText(frame, text, org, font, font_scale, white_color, thickness, cv2.LINE_AA)
+#     write_text("hello", frame, out)
+    
+# for i in range(5 * 30):
+#     ret, frame = cap.read()
+#     if not ret:
+#         break 
+#     write_text("fucking", frame, out)
 
-    out.write(frame)
+# for i in range(5 * 30):
+#     ret, frame = cap.read()
+#     if not ret:
+#         break 
+#     write_text("world", frame, out)
 
 cap.release()
 out.release()
