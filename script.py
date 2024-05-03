102b0f565cc43c3c20e14d3ba0c14a0af79b788c
m.konurov <m.konurov@geeksoft.kz>
2024-05-03 18:57:32 +0500

    sript edit to prevent non-alligned queries

--- script.py
+++ script.py
@@ -1,6 +1,7 @@
 import config
 import narration
 import images
+import video
 
 from openai import OpenAI
 from elevenlabs.client import ElevenLabs
@@ -19,55 +20,62 @@ else:
 with open("source_material.txt") as f:
     source_material = f.read()
 
-response = openai_client.chat.completions.create(
-    model = "gpt-3.5-turbo",
-    messages=[
-        {
-            "role":"system",
-            "content":"""
-            You are a YouTube short narration generator. 
-            You have to create 30 seconds to 1 minute narration.
-            The shorts you create have background that fades from image to image as the narration go on.
-            Your responses should not contain any explicit data, some personal info and so on that can break usual AI allignment rules of generating images, texts, audios, etc.
-            Respond in the following format, repeat until the end of the shorts:
+print("Generating data...")
+# response = openai_client.chat.completions.create(
+#     model = "gpt-3.5-turbo",
+#     messages=[
+#         {
+#             "role":"system",
+#             "content":"""
+#             You are a YouTube short narration generator. 
+#             You have to create 30 seconds to 1 minute narration.
+#             The shorts you create have background that fades from image to image as the narration go on.
+#             Your responses should not contain any explicit data, some personal info and so on that can break usual AI allignment rules of generating images, texts, audios, etc.
+#             DO NOT IN ANY CIRCUMSTANCES ADD SOME SENSITIVE, VIOLENT DATA RELATED TO SOME PERSONS, CITIES, COUNTRIES etc.
+#             Respond in the following format, repeat until the end of the shorts:
 
-            ###
+#             ###
 
-            Background: "5-10 words with the background desription"
+#             Background: "5-10 words with the background desription"
 
-            Narrator: "1 sentence of narration"
+#             Narrator: "1 sentence of narration"
 
-            Background: "5-10 words with the background desription"
+#             Background: "5-10 words with the background desription"
 
-            Narrator: "1 sentence of narration"
+#             Narrator: "1 sentence of narration"
 
-            Background: "5-10 words with the background desription"
+#             Background: "5-10 words with the background desription"
 
-            Narrator: "1 sentence of narration"
+#             Narrator: "1 sentence of narration"
 
-            Background: "5-10 words with the background desription"
+#             Background: "5-10 words with the background desription"
 
-            Narrator: "1 sentence of narration"
+#             Narrator: "1 sentence of narration"
 
-            Background: "5-10 words with the background desription"
+#             Background: "5-10 words with the background desription"
 
-            Narrator: "1 sentence of narration"
+#             Narrator: "1 sentence of narration"
 
-            ###
+#             ###
 
-            """
-        },
-        {
-            "role":"user",
-            "content":f"Create a YouTube short narration based on the following source material:\n\n{source_material}"
-        }
-    ]
-)
+#             """
+#         },
+#         {
+#             "role":"user",
+#             "content":f"Create a YouTube short narration based on the following source material:\n\n{source_material}"
+#         }
+#     ]
+# )
 
-data = narration.parse(response.choices[0].message.content)
+# print("Generating narrations...")
+# data = narration.parse(response.choices[0].message.content)
+# narration.create(client, data, "narrations")
 
-with open("data.json", "w") as f:
-    json.dump(data, f)
+# with open("data.json", "w") as f:
+#     json.dump(data, f)
 
-#narration.create(client, data, "narrations")
-#images.create_from_data(data)
+# print("Generating images...")
+# images.create_from_data(data)
+
+# print("Generating video...")
+video.create("youtube_short.avi")
