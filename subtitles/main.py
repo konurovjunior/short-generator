1459ea039c0de4dc7e51db7b865d0e3219709ae0
m.konurov <m.konurov@geeksoft.kz>
2024-05-03 20:49:19 +0500

    fixing new subtitles

--- subtitles/main.py
+++ subtitles/main.py
@@ -24,10 +24,9 @@ with open("segments.json") as f:
     segments = json.load(f)
 
 words = {}
-
 for segment in segments:
     for word in segment["words"]:
-        words[word["start"]] = word["word"]
+        words[word["start"]] = word["word"] 
 
 print(words)
 
@@ -41,9 +40,12 @@ fourcc = cv2.VideoWriter_fourcc(*'XVID')
 out = cv2.VideoWriter(output_file, fourcc, framerate, (int(cap.get(3)), int(cap.get(4))))
 
 time = 0
+frame_number = 0
 while cap.isOpened():
     ret, frame = cap.read()
+    frame_number += 1
     if not ret:
+        print(f"Broke on {frame_number}")
         break
 
     for start_time, word in words.items():
